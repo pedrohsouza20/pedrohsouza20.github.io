3974197fa2e33d64e695ca30105a56616232839e
Pedro <contato.pedromoc@gmail.com>
2023-02-03 01:31:08 -0300

    Melhorias e adicoes gerais no codigo

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -12,8 +12,37 @@ export class ProjectsComponent implements OnInit {
   public static projects: [];
 
   projects = [
-    { title: 'teste' },
-    { title: 'teste' }
+    {
+      title: 'Social Articles',
+      description: 'An website where people can post some article/question and others can like and comment about it',
+      technologies: [
+        'Angular', 'Express', 'Sequelize', 'TypeScript', 'MySQL'
+      ],
+      status: 'doing',
+      imagePathName: 'blog-with-cms.png',
+      githubRepositoryLink: 'https://github.com/pedrohsouza20/social-articles'
+    },
+    {
+      title: 'Blog with CMS',
+      description: 'A blog for posting articles, with a CMS - Content Management System where the admin can create new articles and new categories',
+      technologies: [
+        'HTML', 'Express', 'Sequelize', 'EJS', 'MySQL'
+      ],
+      status: 'doing',
+      imagePathName: 'blog-with-cms.png',
+      githubRepositoryLink: 'https://github.com/pedrohsouza20/Blog'
+    },
+    {
+      title: 'Ask and Answers',
+      description: 'An website like Yahoo Answers, with questions and answers.',
+      technologies: [
+        'HTML', 'Express', 'Sequelize', 'EJS', 'MySQL'
+      ],
+      status: 'doing',
+      imagePathName: 'blog-with-cms.png',
+      githubRepositoryLink: 'https://github.com/pedrohsouza20/Ask-Answers'
+    },
+
   ];
 
   ngOnInit(): void {
